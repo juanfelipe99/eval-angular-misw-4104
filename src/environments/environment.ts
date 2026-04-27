de1ebc9724c120bbd2fbfdcaf8e04366a4cd5137
juanfelipe <felipe.rivero@hotmail.com>
2026-04-27 15:11:36 -0500

    agregando environment de produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+const USUARIOS_API_ENDPOINT = 'https://gist.githubusercontent.com/caev03/628509e0b3fe41dd44f6a2ab09d81ef9/raw/f847eafbecca47287ff0faec4de1329b874f5711/users.json';
+
+export const environment = {
+    production: true,
+    usuariosApiEndpoint: USUARIOS_API_ENDPOINT,
+};
