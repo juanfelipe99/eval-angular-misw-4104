9344838e45be3b8f63e895edd2d5286e58752d22
juanfelipe <felipe.rivero@hotmail.com>
2026-04-27 15:46:45 -0500

    agregando estilos a listar usuarios

--- src/app/usuarios/usuario-list/usuario-list.ts
+++ src/app/usuarios/usuario-list/usuario-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { Usuarios } from '../usuarios';
 import { Usuario } from '../usuario';
 import { CommonModule } from '@angular/common';
@@ -10,14 +10,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './usuario-list.html',
   styleUrl: './usuario-list.css',
 })
-export class UsuarioList {
-  usuariosArray: Usuario[] = [];
+export class UsuarioList implements OnInit {
+  usuariosArray = signal<Usuario[]>([]);
 
   constructor(private usuarios: Usuarios) {}
 
   getUsuarios() {
     this.usuarios.getUsuarios().subscribe((usuarios) => {
-      this.usuariosArray = usuarios;
+      this.usuariosArray.set(usuarios);
     });
   }
 
