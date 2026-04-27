20e91a18a794731fa2b68d1b1e3990b78a440f2e
juanfelipe <felipe.rivero@hotmail.com>
2026-04-27 15:03:18 -0500

    creación de la interfaz para usuario

--- src/app/usuarios/usuario.ts
+++ src/app/usuarios/usuario.ts
@@ -1,1 +1,12 @@
-export interface Usuario {}
+// Se utiliza una interfaz en lugar de una clase porque no se necesita implementar métodos o propiedades.
+
+export interface Usuario {
+    id: number;
+    username: string;
+    user: string;
+    email: string;
+    avatarUrl: string;
+    role: string;
+    location: string;
+    repoIds: number[];
+}
