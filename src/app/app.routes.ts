dd643c138261d580f3ae03458cf12110b316e5cd
juanfelipe <felipe.rivero@hotmail.com>
2026-04-27 21:29:15 -0500

    agregando home para presentar la evaluación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import { UsuarioList } from './usuarios/usuario-list/usuario-list';
 import { RepositorioList } from './repositorios/repositorio-list/repositorio-list';
 
 export const routes: Routes = [
+  { path: '', component: Home, pathMatch: 'full' },
   { path: 'usuarios', component: UsuarioList },
   { path: 'repositorios', component: RepositorioList },
 ];
